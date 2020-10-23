49bd958be5b7a5299a3468ac7a6f2cb47d048a9e
Olugbenga2000 <41525551+Olugbenga2000@users.noreply.github.com>
2020-10-23 08:17:07 +0100

    Update SupplyChain.sol

--- contracts/SupplyChain.sol
+++ contracts/SupplyChain.sol
@@ -12,11 +12,12 @@ contract SupplyChain {
   address owner;
 
   /* Add a variable called skuCount to track the most recent sku # */
+     uint skuCount;
 
   /* Add a line that creates a public mapping that maps the SKU (a number) to an Item.
      Call this mappings items
   */
-
+   mapping(uint=>Item)public items;
   /* Add a line that creates an enum called State. This should have 4 states
     ForSale
     Sold
@@ -24,20 +25,26 @@ contract SupplyChain {
     Received
     (declaring them in this order is important for testing)
   */
-
+   enum State {ForSale,Sold,Shipped, Received};
   /* Create a struct named Item.
     Here, add a name, sku, price, state, seller, and buyer
     We've left you to figure out what the appropriate types are,
     if you need help you can ask around :)
     Be sure to add "payable" to addresses that will be handling value transfer
   */
-
+   struct Item (string name,uint sku,uint price, State state, address payable seller, address payable buyer);/
   /* Create 4 events with the same name as each possible State (see above)
     Prefix each event with "Log" for clarity, so the forSale event will be called "LogForSale"
     Each event should accept one argument, the sku */
+    event LogForSale(uint sku);
+    event LogSold(uint sku);
+    event LogShipped(uint sku);
+    event LogReceived(uint sku);
 
 /* Create a modifer that checks if the msg.sender is the owner of the contract */
-
+  modifier verifyOwner () { 
+  require (msg.sender == owner); 
+  _;}
   modifier verifyCaller (address _address) { require (msg.sender == _address); _;}
 
   modifier paidEnough(uint _price) { require(msg.value >= _price); _;}
